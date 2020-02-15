e015e7a92f41aa8551c689235191f8f7b97d9db9
johnking99 <john.a.king-1@ou.edu>
2020-02-15 14:37:23 -0600

    Help

---
+++ NameFormat/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NameFormat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Enter first name.");
+            Console.ReadLine();
+            Console.WriteLine("Enter last name");
+            Console.ReadLine();
+            } 
+        }
+    }
+}
