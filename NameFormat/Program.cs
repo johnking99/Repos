8a3d783381e78994bb9d6abbd94030526161ca11
johnking99 <john.a.king-1@ou.edu>
2020-02-16 16:37:07 -0600

    progress1

--- NameFormat/Program.cs
+++ NameFormat/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace NameFormat
 {
@@ -6,13 +7,31 @@ namespace NameFormat
     {
         static void Main(string[] args)
         {
+            //string the name list
+            List<string> ListOfNames = new List<string>();
 
+            //ask user to input names in the list
 
-            Console.WriteLine("Enter first name.");
-            Console.ReadLine();
-            Console.WriteLine("Enter last name");
-            Console.ReadLine();
-            } 
+            Console.WriteLine("Please enter the names you are needing.");
+            string Continue = "Yes";
+            while (Continue != "No"&& Continue!= "no"&& Continue !="n"&& Continue != "N")
+            {
+                Console.WriteLine("Enter first name.");
+                string firstname = Console.ReadLine();
+                Console.WriteLine("Enter last name");
+                string lastname = Console.ReadLine();
+                Console.WriteLine("Are you entering more names? Yes/No");
+                string fullname = "<" + lastname + ", " + firstname + ">";
+                Continue = Console.ReadLine();
+                ListOfNames.Add(fullname);
+            }
+            ListOfNames.Sort();
+            Console.WriteLine("Final List:");
+            foreach (string A in ListOfNames)
+            {
+                Console.WriteLine(A);
+            }
+            Console.ReadKey();
         }
     }
 }
